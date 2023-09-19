0d2d09d68dc37ae65cd93a53185fd418904080df
SamuelFarrel <samuel.farrel@ui.ac.id>
2023-09-19 20:20:35 +0700

    pengerjaan tugas 3

--- main/models.py
+++ main/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField
-    amount = models.IntegerField
-    description = models.TextField
-    car = models.CharField
-    production_date = models.DateField
-    price = models.IntegerField
+    name = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    description = models.TextField()
+    car = models.CharField(max_length=255)
+    production_date = models.DateField(auto_now_add=True)
+    price = models.IntegerField()
