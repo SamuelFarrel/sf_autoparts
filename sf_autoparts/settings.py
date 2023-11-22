bd4a663b35b07ab10b32862126128c613cef7eeb
SamuelFarrel <samuel.farrel@ui.ac.id>
2023-11-22 09:22:58 +0700

    fix csrf tugas 9 flutter

--- sf_autoparts/settings.py
+++ sf_autoparts/settings.py
@@ -142,3 +142,4 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ["https://samuel-farrel-tugas.pbp.cs.ui.ac.id"]
